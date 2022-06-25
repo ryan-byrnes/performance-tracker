95f5292e76f00261194d6dd60baaaec278611e38
Ryan Byrnes <ryn_byrnes@yahoo.com>
2022-06-25 11:46:24 -0700

    adding pr service and model

--- src/personal-records/personal-records.service.ts
+++ src/personal-records/personal-records.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from "@nestjs/common";
+import { PersonalRecords } from "./personal-records.model";
+
 
 @Injectable()
 export class PersonalRecordsService {
+    personalRecords: PersonalRecords[] = [];
 
 }
